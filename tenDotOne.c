b87de00d54292ebee34c849b266423e83bc40999
gomoletz <david.postgrad@gmail.com>
2016-02-06 20:00:33 -0600

    Changed book example.

--- tenDotOne.c
+++ tenDotOne.c
@@ -1,11 +1,11 @@
-int main(int argc, char *argv[]){
-	int valid = FALSE;
+ccint main(int argc, char *argv[]){
+	int valid = 0;
 	char str1[8];
 	char str2[8];
 	
 	next_tag(str1);
 	gets(str2);
 	if (strncmp(str1, str2, 8) == 0)
-		valid = TRUE;
+		valid = 1;
 	printf("buffer1: str1(%s), str2(%s), valid(%d)\n", str1, str2, valid);
 }
